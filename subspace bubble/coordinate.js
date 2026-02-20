96fc337e5470687e1c1a5527202b46f6091ef77a
SnappyRiffs <jadonjung3@gmail.com>
2026-02-20 15:50:42 -0800

    Refactor mycoor function to calculate_distance

--- subspace bubble/coordinate.js
+++ subspace bubble/coordinate.js
@@ -1,11 +1,11 @@
-function mycoor() {
+function calculate_distance() {
 	var x = parseInt(document.getElementsByClassName('x')[0].value);
 	var z = parseInt(document.getElementsByClassName('z')[0].value);
-	var check = document.forms["coor"]["x"].value;
-	var check_two = document.forms["coor"]["z"].value;
+	var x_coord_check = document.forms["coor"]["x"].value;
+	var z_coord_check = document.forms["coor"]["z"].value;
 	var result = (x + 700) + " " + (z + 700);
 
-	if ((check == null || check == "") || (check_two == null || check_two == "")) {
+	if ((x_coord_check == null || x_coord_check == "") || (z_coord_check == null || z_coord_check == "")) {
 		//checks if both or one of the inputs are emtpy
 		confirm("Check your inputs");
 	}
@@ -13,5 +13,6 @@ function mycoor() {
 
 		document.getElementById('result').innerHTML = "Coordinates are " + result;
 	}
+
 
 } 

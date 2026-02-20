d1704927e803312ae6dc375a51ed7b2983834f44
SnappyRiffs <jadonjung3@gmail.com>
2026-02-20 15:51:46 -0800

    Refactor myRand function for clarity

--- random/random.js
+++ random/random.js
@@ -1,19 +1,17 @@
-
-
-
 function myRand() {
 	var min = parseInt(document.getElementsByClassName('min')[0].value);
 	var max = parseInt(document.getElementsByClassName('max')[0].value);
-	var check = document.forms["rand"]["min"].value;
-	var check_two = document.forms["rand"]["max"].value;
+	var min_check = document.forms["rand"]["min"].value;
+	var max_check = document.forms["rand"]["max"].value;
 	var result = Math.floor(Math.random() * (max - min + 1)) + min;
 
-	if ((check == null || check == "") || (check_two == null || check_two == "")) {
+	if ((min_check == null || min_check == "") || (max_check == null || max_check == "")) {
 		confirm("Check your inputs");
 	}
 	else {
 
 		document.getElementById('result').innerHTML = "Number is " + result;
 	}
+
 
 } 
